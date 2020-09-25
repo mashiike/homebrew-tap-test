d1e3a8a099435c1013f393ce4dbf31db8dd8da52
mashiike <ikeda-masashi@kayac.com>
2020-09-25 14:13:21 +0900

    beta is bump

--- yaml2text@0.1.0.rb
+++ yaml2text@0.1.0.rb
@@ -1,4 +1,4 @@
-class Yaml2textATbeta < Formula
+class Yaml2textAT010 < Formula
     version '0.1.0'
     homepage 'https://github.com/mashiike/yaml2text'
     url "https://github.com/mashiike/yaml2text/releases/download/v0.1.0/yaml2text-v0.1.0-darwin-amd64"
