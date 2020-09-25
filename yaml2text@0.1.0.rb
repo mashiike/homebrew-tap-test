a387f4eaa2b4c2a6c1ce12814b377d8a228cd05d
mashiike <ikeda-masashi@kayac.com>
2020-09-25 14:02:21 +0900

    switch version test

---
+++ yaml2text@0.1.0.rb
@@ -0,0 +1,18 @@
+class Yaml2textATOld < Formula
+    version '0.1.0'
+    homepage 'https://github.com/mashiike/yaml2text'
+    url "https://github.com/mashiike/yaml2text/releases/download/v0.1.0/yaml2text-v0.1.0-darwin-amd64"
+    sha256 '7d80b8936c60af2fdd0ac4e686484460cfa31288e209a10ec512f644558ec9c2'
+    head 'https://github.com/mashiike/yaml2text.git'
+
+    head do
+      depends_on 'go' => :build
+    end
+
+    def install
+      if build.head?
+        system 'make', 'build'
+      end
+      bin.install 'yaml2text'
+    end
+  end
